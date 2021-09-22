13c3f2463e30ca194cd07fba31d77d1d31433f51
susantubokumetzger <tuboku-metzger.s@northeastern.edu>
2021-09-21 22:19:53 -0400

    assignment1

---
+++ NetBeansProjects/Assignmentfinal/src/model/Personalprofile.java
@@ -0,0 +1,143 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+/**
+ *
+ * @author susan
+ */
+public class Personalprofile {
+      
+    private String name;
+    private String geodata;
+    private String DOB;
+    private String tele;
+    private String fax;
+    private String email;
+    private String SSN;
+    private String medicalnum;
+    private String Banknum;
+    private String licensenum;
+    private String vehicleidentity;
+    private String linkedIn;
+    private String IPO;
+    private String identitynum;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getGeodata() {
+        return geodata;
+    }
+
+    public void setGeodata(String geodata) {
+        this.geodata = geodata;
+    }
+
+    public String getDOB() {
+        return DOB;
+    }
+
+    public void setDOB(String DOB) {
+        this.DOB = DOB;
+    }
+
+    public String getTele() {
+        return tele;
+    }
+
+    public void setTele(String tele) {
+        this.tele = tele;
+    }
+
+    public String getFax() {
+        return fax;
+    }
+
+    public void setFax(String fax) {
+        this.fax = fax;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSSN() {
+        return SSN;
+    }
+
+    public void setSSN(String SSN) {
+        this.SSN = SSN;
+    }
+
+    public String getMedicalnum() {
+        return medicalnum;
+    }
+
+    public void setMedicalnum(String medicalnum) {
+        this.medicalnum = medicalnum;
+    }
+
+    public String getBanknum() {
+        return Banknum;
+    }
+
+    public void setBanknum(String Banknum) {
+        this.Banknum = Banknum;
+    }
+
+    public String getLicensenum() {
+        return licensenum;
+    }
+
+    public void setLicensenum(String licensenum) {
+        this.licensenum = licensenum;
+    }
+
+    public String getVehicleidentity() {
+        return vehicleidentity;
+    }
+
+    public void setVehicleidentity(String vehicleidentity) {
+        this.vehicleidentity = vehicleidentity;
+    }
+
+    public String getLinkedIn() {
+        return linkedIn;
+    }
+
+    public void setLinkedIn(String linkedIn) {
+        this.linkedIn = linkedIn;
+    }
+
+    public String getIPO() {
+        return IPO;
+    }
+
+    public void setIPO(String IPO) {
+        this.IPO = IPO;
+    }
+
+    public String getIdentitynum() {
+        return identitynum;
+    }
+
+    public void setIdentitynum(String identitynum) {
+        this.identitynum = identitynum;
+    }
+    
+    
+    
+}
